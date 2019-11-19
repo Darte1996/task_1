2f0ba68384f4537a9a3142db75e633941175ff01
Darte1996 <Darte1996@mail.ru>
2019-11-19 17:48:43 +0300

    Initial commit

---
+++ src/LearnMain.java
@@ -0,0 +1,8 @@
+public class LearnMain {
+    public static void main(final String... args) {
+        System.out.println("Hello Java");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 11);
+
+    }
+}
